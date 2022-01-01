ae88dc80667ca7f0e52c834d37a3672aca0bff18
Tamago <yukotaniguchi@outlook.com>
2022-01-01 22:41:45 +1100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const accordion=document.getElementsByClassName("card__accordion");
+
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+
+      /* Toggle between adding and removing the "active" class,
+      to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+
+
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.nextElementSibling;
+      const arrow=this.children[0];
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+        arrow.classList.remove("arrow__up");
+      } else {
+        panel.style.display = "block";
+        arrow.classList.add("arrow__up")
+      }
+    });
+  }
