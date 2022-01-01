fb65ab59945287c9c094f5da1cf5df880fd14759
Tamago <yukotaniguchi@outlook.com>
2022-01-02 09:12:34 +1100

    layout change

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ for (i = 0; i < accordion.length; i++) {
       to highlight the button that controls the panel */
       this.classList.toggle("active");
 
-
-
       /* Toggle between hiding and showing the active panel */
       var panel = this.nextElementSibling;
       const arrow=this.children[0];
